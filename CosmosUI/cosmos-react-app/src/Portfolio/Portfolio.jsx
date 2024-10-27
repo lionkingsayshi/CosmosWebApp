80ccc6d7f421a742e369e25a2d349bb2e6442a51
Souvik Mukherjee <souvik01997@outlook.com>
2024-10-27 10:40:35 +0530

    fix/components
    fix: margin alignments

--- CosmosUI/cosmos-react-app/src/Portfolio/Portfolio.jsx
+++ CosmosUI/cosmos-react-app/src/Portfolio/Portfolio.jsx
@@ -1,3 +1,5 @@
+// src/Portfolio.jsx
+
 import React, { useState } from 'react';
 import '../CSS/Portfolio.css'; // Ensure your CSS is included
 import Experience from '../components/Experience';
@@ -15,7 +17,7 @@ const Portfolio = () => {
       <div>
         <h2>About Me</h2>
         <p>Hi, I’m Souvik! I'm a software developer with in-depth experience in Asp.net core and React development and design. In a nutshell, I create application and API's that help organizations address business challenges and meet their needs.</p>
-        <p>At Societe Generale Global Solution Centre, my role as a Software Engineer centers around developing resilient software that leverages the full stack of .Net Core, complemented by front-end innovation with React. Our team's commitment to cloud technologies like AWS and SG Cloud, coupled with an agile approach to DevOps, has been integral to our digital transformation efforts..</p>
+        <p>At Societe Generale Global Solution Centre, my role as a Software Engineer centers around developing resilient software that leverages the full stack of .Net Core, complemented by front-end innovation with React. Our team's commitment to cloud technologies like AWS and SG Cloud, coupled with an agile approach to DevOps, has been integral to our digital transformation efforts.</p>
         <p>My educational grounding in Computer Science from Chandigarh University has equipped me with a robust foundation in object-oriented programming and Azure DevOps Services. With a passion for continuous learning, I've augmented my skill set with certifications in Machine Learning and Reinforcement Learning, which guide my contributions to creating intelligent, scalable solutions.</p>
       </div>
     ),
